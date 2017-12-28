c357b8992ad284ce63c797f24716d66c32336ee4
youlexia <34920044+youlexia@users.noreply.github.com>
2017-12-28 23:19:08 +0200

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,32 @@
+var game = 0;
+
+var games = [
+	{ url: "http://www.nickjr.tv/gsp.scenic/international/games/nella-connect-the-shots/build/index.html", w: 450, h: 265, instruction: "" },
+	{ url: "http://kidmons.com/game/paint-online", w: 450, h: 300, instruction: "נא לשרבל חופשי" },
+];
+ 
+ 
+function gameplace(n){
+	if (n !== undefined) {
+		game = n;
+	}
+
+	if (game == games.length){
+		game = 0;
+	}
+	
+	return games[game++];
+ }
+ 
+ function setUrl() {
+	var game = gameplace();
+	 $('iframe').attr('src', game.url).attr('width', game.w).attr('height', game.h);
+	 $('#instruct').text(game.instruction);
+ }
+ function finish(){
+	 setUrl();
+ }
+ function init() {
+	 $('#next').click(finish);
+	 setUrl();
+ }
